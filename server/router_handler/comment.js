1b1eb8f67418a88b4dd94279d15aca8a22c7f14b
邓爱琪 <magic_8@qq.com>
2024-11-06 17:23:19 +0800

    comment

--- server/router_handler/comment.js
+++ server/router_handler/comment.js
@@ -4,6 +4,17 @@
 
 const db = require('../db/index')
 
+// 封装数据库查询函数
+function queryDatabase(sql, params, callback) {
+    db.query(sql, params, function (err, results) {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, results);
+    });
+}
+
+
 //评论功能的封装
 exports.comment = (req, res) => {
     console.log(req.body);
